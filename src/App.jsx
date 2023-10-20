3f245abc27a5d8e1c28edda1cb698b6d83fcb6fe
dylangangat <dylan.gangat@gmail.com>
2023-10-20 17:20:14 +0200

    feat: Add Vite and Tailwind CSS setup to project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="mx-auto p-8 text-3xl font-bold text-orange-600 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
